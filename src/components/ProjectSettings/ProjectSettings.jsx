86bc23b4f7e9adee14f03dc0ab29cce1257946fc
rafaelt7 <rafaelt@cetys.edu.mx>
2019-10-20 21:28:37 -0700

    Cambios a vista de configuracion de proyecto

--- src/components/ProjectSettings/ProjectSettings.jsx
+++ src/components/ProjectSettings/ProjectSettings.jsx
@@ -6,64 +6,69 @@ class ProjectSettings extends Component {
   state = {};
   render() {
     return (
-      <div className="center settings">
+      <div className="center w-100">
         <h1 className="settings-title">Configuracion de Nuevo Proyecto</h1>
+        <div className="settings settings-stripe p-4">
+          <div className="settings-input">
+            <div className="input-group mb-3">
+              <div className="input-group-prepend col-5 px-0">
+                <span className="input-group-text w-100" id="basic-addon1">
+                  Nombre del Proyecto
+                </span>
+              </div>
+              <input
+                type="text"
+                className="form-control col-7"
+                placeholder="Nombre"
+                aria-label="Username"
+                aria-describedby="basic-addon1"
+              />
+            </div>
 
-        <div className="input-group mb-3">
-          <div className="input-group-prepend">
-            <span className="input-group-text" id="basic-addon1">
-              Nombre del Proyecto
-            </span>
+            <div className=" no-gutter input-group mb-3">
+              <div className="input-group-prepend col-5 px-0">
+                <span className="input-group-text w-100" id="basic-addon1">
+                  Alto
+                </span>
+              </div>
+              <input
+                type="number"
+                className="form-control col-7 "
+                placeholder="8"
+                aria-label="Username"
+                aria-describedby="basic-addon1"
+                min="8"
+                max="128"
+              />
+            </div>
+
+            <div className="input-group mb-3">
+              <div className="input-group-prepend col-5 px-0">
+                <span className="input-group-text w-100" id="basic-addon1">
+                  Ancho
+                </span>
+              </div>
+              <input
+                type="number"
+                className="form-control col-7"
+                placeholder="8"
+                aria-label="Username"
+                aria-describedby="basic-addon1"
+                min="8"
+                max="128"
+              />
+            </div>
+
+            <button
+              className="btn btn-primary btn-sm m-2"
+              onClick={() =>
+                this.props.onMoveToProject(this.props.LoadedComponent)
+              }
+            >
+              Crear Proyecto
+            </button>
           </div>
-          <input
-            type="text"
-            className="form-control "
-            placeholder="Nombre"
-            aria-label="Username"
-            aria-describedby="basic-addon1"
-          />
         </div>
-
-        <div className="input-group mb-3">
-          <div className="input-group-prepend">
-            <span className="input-group-text" id="basic-addon1">
-              Alto
-            </span>
-          </div>
-          <input
-            type="number"
-            className="form-control "
-            placeholder="8"
-            aria-label="Username"
-            aria-describedby="basic-addon1"
-            min="8"
-            max="128"
-          />
-        </div>
-
-        <div className="input-group mb-3">
-          <div className="input-group-prepend">
-            <span className="input-group-text" id="basic-addon1">
-              Ancho
-            </span>
-          </div>
-          <input
-            type="number"
-            className="form-control "
-            placeholder="8"
-            aria-label="Username"
-            aria-describedby="basic-addon1"
-            min="8"
-            max="128"
-          />
-        </div>
-
-        <button
-          className="btn btn-primary btn-sm m-2"
-          onClick={() => this.props.onMoveToProject(this.props.LoadedComponent)}
-        >
-          Crear Proyecto
-        </button>
       </div>
     );
   }
