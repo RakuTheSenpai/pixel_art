7dcb723cdbbbf56f36186c62a29c51a9e289af4b
Jose Luis <jose0luisjc7@gmail.com>
2019-11-17 14:32:18 -0800

    Leer archivo de text y crear el objeto ProjectData

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -10,6 +10,7 @@ class App extends Component {
   state = {
     LoadedComponent: 1,
     ProjectData: {}
+
   };
   render() {
     return (
@@ -24,8 +25,13 @@ class App extends Component {
     if (this.state.LoadedComponent === 1) {
       return (
         <Menu
+          id="Menu"
+          ref="Menu"
+          ProjectData={this.state.ProjectData}
           LoadedComponent={this.state.LoadedComponent}
           onMoveToProjectSettingsView={this.handleMoveToProjectSettingsView}
+          onOpenProject={this.handleOpenProject}
+          onChange={this.handleChange}
         />
       );
     } else if (this.state.LoadedComponent === 2) {
@@ -42,19 +48,81 @@ class App extends Component {
   }
 
   handleMoveToProjectSettingsView = LoadedComponent => {
-    console.log("Hi");
     this.setState({ LoadedComponent: 2 });
   };
 
   handleMoveToProject = LoadedComponent => {
-    console.log("Hello");
     this.setState({ LoadedComponent: 3 });
   };
 
+  //////////////////////////
   updateProjectData = data => {
-    // console.log(data);
     this.setState({ ProjectData: data });
   };
+
+  handleOpenProject = LoadedComponent => {
+    var input = this.refs.Menu.refs.FileInput;
+    input.click();
+    console.log(input);
+  }
+
+  readAsText = (inputFile) => {
+    const reader = new FileReader();
+
+    return new Promise((resolve, reject) => {
+
+      reader.onerror = () => {
+        reader.abort();
+        reject(new DOMException("Problem parsing input file"));
+      };
+
+      reader.onload = () => {
+        resolve(reader.result);
+      };
+
+      reader.readAsText(inputFile);
+    })
+  }
+
+  handleChange = async event => {
+
+    let file = event.target.files[0]
+
+
+    try {
+      console.log("Hola");
+      const TextFile = await this.readAsText(file);
+      console.log(TextFile);
+
+      var ProjectData = JSON.parse(TextFile);
+      console.log(ProjectData);
+
+      if (!ProjectData.hasOwnProperty("nombre") ||
+        !ProjectData.hasOwnProperty("height") ||
+        !ProjectData.hasOwnProperty("width") ||
+        !ProjectData.hasOwnProperty("numpixels") ||
+        !ProjectData.hasOwnProperty("pixels")) {
+
+        throw { message: "Formato del archivo de entrada incorrecto" };
+
+      } else {
+
+        this.setState({ ProjectData: ProjectData.nombre });
+        this.setState({ ProjectData: ProjectData.height });
+        this.setState({ ProjectData: ProjectData.width });
+        this.setState({ ProjectData: ProjectData.numpixels });
+        this.setState({ ProjectData: ProjectData.pixels });
+
+      }
+
+    } catch (e) {
+      console.warn(e.message);
+      alert(e.message);
+    }
+
+
+  }
+
 }
 
 export default App;
